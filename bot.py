e239840b26dbbea42c2dfd11ff84a9fe13aeea6c
javohirbot2 <97433981+javohirbot2@users.noreply.github.com>
2022-01-13 01:29:14 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from googletrans import Translator
 updater = Updater('5009722252:AAFla1n_v5_zD_DgVOTkQinv8QtUCUAMTIY',use_context = True )
 
 def start(updater,context):
- updater.message.reply_text("👋🏻 Salom botga hush kelibsiz!\nBotdan foydalanish uchun\n@foydali_link_1 kanaliga a'zo\nbo'lib qayta /start bosing  ")
+ updater.message.reply_text("👋🏻 Salom botga hush kelibsiz!\nBotdan foydalanish uchun\n@foydali_link_1 kanaliga a'zo\nbo'lib qayta /start bosing!  ")
  
 def echo(updater,context):
  usr_msg =updater.message.text
