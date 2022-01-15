0ac820f3371606efcb67090d7f6287e93f2d2413
javohirbot2 <97433981+javohirbot2@users.noreply.github.com>
2022-01-15 17:36:54 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ updater = Updater('5009722252:AAFla1n_v5_zD_DgVOTkQinv8QtUCUAMTIY',use_context =
 
 def start(updater,context):
  updater.message.reply_text("👋🏻 Salom botga hush kelibsiz!\nBotdan foydalanish uchun\n@foydali_link_1 kanaliga a'zo bo'lib \nqayta /start bosing!  ")
-def start(updater,start):
+ 
+def start(updater,constart):
  updater.message.reply_text(" Endi botdan be'malol foydalanishingiz mumkin! ")
  
 def echo(updater,context):
